fc96cc74f15772c76447554f651335c6758439ea
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-09-22 23:41:37 +0200

    ipc zum Anzeigen des eingegebenen Wertes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const electron = require('electron')
 const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
 const axios = require('axios')
+const ipc = electron.ipcRenderer
 
 const notifyBtn = document.getElementById('notifyBtn')
 var price = document.querySelector('h1')
@@ -26,4 +27,9 @@ notifyBtn.addEventListener('click', function(event) {
     win.show()
 
 
+})
+
+ipc.on('targetPriceVal', function (event, arg) {
+    targetPriceVal = Number(arg)
+    targetPrice.innerHTML = '$' + targetPriceVal.toLocaleString('en')
 })
